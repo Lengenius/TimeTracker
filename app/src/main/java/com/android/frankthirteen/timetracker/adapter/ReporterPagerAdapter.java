18bad2a9bab3934bebdf0d8741026386a3516d72
VenMia0 <jkbentaylor@126.com>
2016-06-14 22:51:09 +0800

    add PagerTabStrip but not as wished. may use PagerViewIndicator instead

--- app/src/main/java/com/android/frankthirteen/timetracker/adapter/ReporterPagerAdapter.java
+++ app/src/main/java/com/android/frankthirteen/timetracker/adapter/ReporterPagerAdapter.java
@@ -7,6 +7,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import com.android.frankthirteen.timetracker.fragment.ReporterChartFragment;
 import com.android.frankthirteen.timetracker.fragment.ReporterModifyFragment;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,11 +18,11 @@ public class ReporterPagerAdapter extends FragmentPagerAdapter {
 
     private List<Fragment> fragments;
 
-    private UUID trackerId;
-
-    public ReporterPagerAdapter(FragmentManager fm,UUID uuid) {
+    public ReporterPagerAdapter(FragmentManager fm, UUID uuid) {
         super(fm);
-
+        if (fragments == null) {
+            fragments = new ArrayList<Fragment>();
+        }
         fragments.add(ReporterChartFragment.newInstance(uuid));
         fragments.add(ReporterModifyFragment.newInstance(uuid));
     }
@@ -29,20 +30,17 @@ public class ReporterPagerAdapter extends FragmentPagerAdapter {
     @Override
     public Fragment getItem(int position) {
 
-        Fragment fragment;
-
-
-        return fragment;
+        return fragments.get(position);
     }
 
     @Override
     public int getCount() {
-        return 2;
+        return fragments.size();
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        switch (position){
+        switch (position) {
             case 0:
                 return "ChartView";
             case 1:
